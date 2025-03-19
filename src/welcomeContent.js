23ebb9134b71c42205fe03556b8e44164a69f7dd
noveira <amylopez303@gmail.com>
2025-03-19 18:32:08 -0400

    feat: Added modular welcome content file

---
+++ src/welcomeContent.js
@@ -0,0 +1,86 @@
+const welcomeContent = `# Bienvenido a WriteMD
+
+👋 ¡Hola! te doy la bienvenida a WriteMD, un editor de Markdown que he creado para que puedas escribir y previsualizar tu contenido en tiempo real de manera sencilla y eficiente. 
+
+## ⚙️ Funcionalidades principales:
+- Escribe Markdown en el panel izquierdo y observa cómo se transforma en contenido formateado.
+
+- Ve el resultado de tu Markdown al instante en el panel derecho.
+
+- Organiza tu trabajo con múltiples pestañas ¡Crea, edita o elimina según tus necesidades!
+
+- Tus cambios se guardan automáticamente, para que nunca pierdas tu progreso.
+
+## ¿Qué es Markdown?
+Markdown es un lenguaje de marcado ligero y fácil de usar. Es perfecto para:
+
+- Crear documentos bien estructurados.
+
+- Escribir blogs o artículos.
+
+- Tomar notas técnicas o personales.
+
+- Formatear texto para plataformas como GitHub, Reddit o Slack.
+
+## 🚀 Ejemplos rápidos de Markdown
+### Encabezados
+
+# Título H1
+## Título H2
+### Título H3
+#### Título H4
+
+---
+
+## Texto básico
+
+- **Negrita**: \`**texto en negrita**\`
+- *Itálica*: \`*texto en itálica*\`
+- ~~Tachado~~: \`~~texto tachado~~\`
+
+### Listas
+
+- Lista desordenada:
+  - Elemento 
+  - Elemento 
+
+1. Lista ordenada:
+   1. Primer elemento
+   2. Segundo elemento
+
+## Enlaces e imágenes
+
+[Enlace a mi Github](https://www.google.com)
+#### Logo de WriteMD
+![](https://pandao.github.io/editor.md/images/logos/editormd-logo-180x180.png)
+
+## Código
+
+- En línea: \`console.log("Hola, mundo");\`
+- En bloque:
+  \`\`\`javascript
+  function saludar() {
+    console.log("¡Hola, mundo desde WriteMD!");
+  }
+  \`\`\`
+
+### Tablas
+
+| Nombre  | Edad | Ciudad     |
+|---------|------|------------|
+| Juan    | 25   | Madrid     |
+| María   | 30   | Barcelona  |
+
+# 💜 ¡Apóyame!
+
+Si te gusta lo que ves, ¡me encantaría contar con tu apoyo! 🚀
+
+⭐ Si WriteMD te resulta útil, dale una estrella en mi repositorio de [Github](https://github.com/noveiradev/write-md). ¡Significaría mucho para mí!
+
+🛠️ Si tienes ideas, sugerencias o quieres colaborar, ¡no dudes en abrir un issue o enviar un pull request! Estoy abierta a mejoras y nuevas funcionalidades.
+
+🐛 ¿Encontraste un bug? ¡Házmelo saber! Quiero que WriteMD sea lo más estable y útil posible.
+`;
+
+export default welcomeContent;
+
